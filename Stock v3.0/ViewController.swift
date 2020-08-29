b7f78efff0084e1ba83cc3a9e44d8d9ba88bd36a
Artur <Gedakyan>
2020-08-29 12:30:06 +0300

    Initial Commit

---
+++ Stock v3.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock v3.0
+//
+//  Created by Artur Gedakyan on 29.08.2020.
+//  Copyright © 2020 Artur Gedakyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
